6c7ae84294f8447766cc43910f517a549cd48f5f
suvrowebthree <suvro.ghosh.contact@gmail.com>
2023-08-07 17:02:11 +0530

    v3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -58,10 +58,7 @@
 				height="315"
 				src="https://www.youtube.com/embed/2gLRFuX5yNE"
 				title="YouTube video player"
-				frameborder="0"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-				allowfullscreen
-				data-preload
+				frameborder="5"
 			/>
 		</section>
 	</article>
