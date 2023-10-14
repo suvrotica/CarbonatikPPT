6889b3de29bcad42f0057e89d2548a243803c2a0
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-10-14 10:33:30 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,7 @@
             hash: true,
             controls: true,
             progress: true,
-controlsLayout: 'bottom-right',
+controlsLayout: 'edges',
         markdown: {
             smartypants: true
         }
